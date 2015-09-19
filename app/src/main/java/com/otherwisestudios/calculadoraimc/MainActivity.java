3c0039c582c05827198eff2148f46a345198a3d2
Juan Camilo Guarin P <jcguarinpenaranda@gmail.com>
2015-09-19 18:36:38 -0500

    Finished design

--- app/src/main/java/com/otherwisestudios/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/otherwisestudios/calculadoraimc/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_peso, et_altura, et_nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
