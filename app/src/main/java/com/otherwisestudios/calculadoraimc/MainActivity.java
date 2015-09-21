cede43010f372f6607fdbc7420ec25b4d5ba9a62
Juan Camilo Guarin P <jcguarinpenaranda@gmail.com>
2015-09-21 09:45:26 -0500

    Finished IMC calculations with IMC table

--- app/src/main/java/com/otherwisestudios/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/otherwisestudios/calculadoraimc/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        et_peso = (EditText) findViewById(R.id.et_peso);
+        et_altura = (EditText) findViewById(R.id.et_altura);
+        et_nombre = (EditText) findViewById(R.id.et_nombre);
+        et_resultadovalor = (EditText) findViewById(R.id.et_resultadovalor);
+
+        tv_resultadodescripcion = (TextView) findViewById(R.id.tv_resultadodescripcion);
 
     }
 
@@ -44,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void calcular(){
+    public void calcular(View view){
 
         /*
         * IMC = peso / altura*altura
@@ -52,6 +61,35 @@ public class MainActivity extends AppCompatActivity {
          * IMC = 69 / 1,69²
         * */
 
+        float IMC = 0;
+        //NumberFormat format ;
+
+        float peso = Float.parseFloat(et_peso.getText().toString());
+
+        float altura = Float.parseFloat(et_altura.getText().toString());
+
+        altura/=100; //para pasar de cm a metros
+
+        IMC =  peso / (altura*altura);
+
+        et_resultadovalor.setText(String.valueOf(IMC));
+
+        int descripcion_val_id = R.string.obesidad3;
+        if(IMC<17){
+            descripcion_val_id = R.string.muybajopeso;
+        }else if(IMC>=17 && IMC <18.5){
+            descripcion_val_id = R.string.bajopeso;
+        }else if(IMC>=18.5 && IMC < 25){
+            descripcion_val_id = R.string.pesonormal;
+        }else if(IMC>=25 && IMC < 30){
+            descripcion_val_id = R.string.sobrepeso;
+        }else if(IMC>=30 && IMC < 35){
+            descripcion_val_id = R.string.obesidad1;
+        }else if(IMC>=35 && IMC <40){
+            descripcion_val_id = R.string.obesidad2;
+        }
+
+        tv_resultadodescripcion.setText(descripcion_val_id);
 
     }
 
